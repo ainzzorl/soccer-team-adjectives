ca034a01e137ae2cdc205998290406b396e0e6d0
ainzzorl <ainzzorl@gmail.com>
2017-06-26 20:55:58 -0700

    Exclude numeric orders

--- lib/soccer_team_adjectives/adjective_filter.rb
+++ lib/soccer_team_adjectives/adjective_filter.rb
@@ -3,9 +3,10 @@ module SoccerTeamAdjectives
   class AdjectiveFilter
     BLACKLIST = %w[spanish german english french italian russian].freeze
     SCORE_REGEX = Regexp.new('^[0-9]+-[0-9]+$').freeze
+    NUMERIC_ORDER_REGEX = Regexp.new('^[0-9]+(th|st|nd|rd)$').freeze
 
     def exclude?(adjective)
-      blacklisted?(adjective) || score?(adjective)
+      blacklisted?(adjective) || score?(adjective) || numeric_order?(adjective)
     end
 
     private
@@ -17,6 +18,12 @@ module SoccerTeamAdjectives
     # rubocop:disable DoubleNegation
     def score?(adjective)
       !!(adjective =~ SCORE_REGEX)
+    end
+    # rubocop:enable DoubleNegation
+
+    # rubocop:disable DoubleNegation
+    def numeric_order?(adjective)
+      !!(adjective =~ NUMERIC_ORDER_REGEX)
     end
     # rubocop:enable DoubleNegation
   end
--- spec/adjective_filter_spec.rb
+++ spec/adjective_filter_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe SoccerTeamAdjectives::AdjectiveFilter do
       %w[1-1 7-3 0-0 11-12].each { |a| expect(filter.exclude?(a)).to be true }
     end
 
+    it 'excludes numeric orders' do
+      %w[4th 9th 11th 1st 2nd 3rd].each { |a| expect(filter.exclude?(a)).to be true }
+    end
+
     it 'does not exclude non-blacklisted words' do
       %w[good bad normal].each { |a| expect(filter.exclude?(a)).to be false }
     end
