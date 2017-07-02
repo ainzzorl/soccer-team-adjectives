fdaef67463865a6be4aef688ac6f1204fb025aa8
ainzzorl <ainzzorl@gmail.com>
2017-07-02 16:05:00 -0700

    Combine finding most common adjectives and filtering them

--- scripts/phase2/filter-adjectives.rb
+++ scripts/phase2/filter-adjectives.rb
@@ -1,14 +1,26 @@
 require 'json'
 require 'yaml'
 
+LIMIT = 100
+
 Dir[File.dirname(__FILE__) + '/../../lib/soccer_team_adjectives/*.rb'].each { |file| require file }
 
 config = YAML.load_file './config/config.yaml'
 
-common_adjectives = File.open('./tmp/phase2/most-common-adjectives.dat').readlines.map do |l|
-  l.sub("\n", '')
-end
 data = JSON.parse(IO.read('./tmp/phase1/reduced.json'))
+
+adjective_counts = {}
+
+data.keys.each do |team|
+  data[team].each do |p|
+    adjective = p['adjective']
+    count = p['count']
+    adjective_counts[adjective] = 0 unless adjective_counts.key?(adjective)
+    adjective_counts[adjective] = adjective_counts[adjective] + count
+  end
+end
+
+common_adjectives = adjective_counts.sort { |a, b| b[1] <=> a[1] }.map { |a| a[0] }.take(LIMIT)
 
 filter = SoccerTeamAdjectives::AdjectiveFilter.new(config)
 
