80debdd798cd97efd9c4e2299f30a6fcdc1d0cc4
ainzzorl <ainzzorl@gmail.com>
2017-07-04 13:18:30 -0700

    Cleanup and add comments to map.rb

--- scripts/phase1/map.rb
+++ scripts/phase1/map.rb
@@ -1,11 +1,13 @@
-# TODO: comment
+# For each comment in the data set
+# find out what teams are mentioned in it and what adjectives are used around them.
+#
+# This is by far the slowest step in the entire process.
+# It can be parallelized if it becomes too slow.
 
 require 'yaml'
-require 'treat'
 require 'set'
 require 'json'
 require 'csv'
-include Treat::Core::DSL
 
 Dir[File.dirname(__FILE__) + '/../../lib/soccer_team_adjectives/*.rb'].each { |file| require file }
 
@@ -17,12 +19,16 @@ comment_data_extractor = SoccerTeamAdjectives::CommentDataExtractor.new(team_nam
 
 output_file = File.open(OUTPUT_FILE_PATH, 'w')
 
-# TODO: comment
-csv_lines = CSV.read(INPUT_FILE_PATH)
-total_comments = csv_lines.size
+# Reading the entire dataset at once rather than per row
+# because it allows to show the progress.
+# Note that a comment can span over more than one line in the file,
+# so can't use something like wc to find out the total number of comments in the dataset.
+# The entire dataset is less than 1G and fits comfortably in RAM.
+csv_rows = CSV.read(INPUT_FILE_PATH)
+total_comments = csv_rows.size
 
 start_time = Time.now
-csv_lines.each_with_index do |comment, comment_index|
+csv_rows.each_with_index do |comment, comment_index|
   if (comment_index % 1000).zero?
     puts "Line #{comment_index}/#{total_comments}, #{(comment_index * 100.to_f / total_comments).round(2)}%," \
       " time elapsed: #{Time.now - start_time}"
