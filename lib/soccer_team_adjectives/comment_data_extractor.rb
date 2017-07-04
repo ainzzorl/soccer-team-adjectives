ef4249a4700688db5ab87c7a0d2b3c775da2259c
ainzzorl <ainzzorl@gmail.com>
2017-07-04 14:04:22 -0700

    Simplify comment data extractor

--- lib/soccer_team_adjectives/comment_data_extractor.rb
+++ lib/soccer_team_adjectives/comment_data_extractor.rb
@@ -13,23 +13,33 @@ module SoccerTeamAdjectives
       paragraph(comment_body).segment.each do |segment|
         teams = @team_name_extractor.extract(segment)
         next if teams.empty?
-        segment.apply :category
-        adjectives = segment.adjectives.map { |a| a.to_s.downcase }
+        adjectives = extract_adjectives(segment)
         next if adjectives.empty?
 
-        teams.each do |team|
-          team_adjective_counts[team] = {} unless team_adjective_counts.key?(team)
-          team_counts = team_adjective_counts[team]
-          adjectives.each do |adjective|
-            count = team_counts[adjective]
-            team_counts[adjective] = count.nil? ? 1 : count + 1
-          end
-        end
+        add_adjectives_to_teams(team_adjective_counts, teams, adjectives)
       end
       {
         comment_id: comment_id,
         team_adjectives: team_adjective_counts
       }
+    end
+
+    private
+
+    def add_adjectives_to_teams(team_adjective_counts, teams, adjectives)
+      teams.each do |team|
+        team_adjective_counts[team] = {} unless team_adjective_counts.key?(team)
+        team_counts = team_adjective_counts[team]
+        adjectives.each do |adjective|
+          count = team_counts[adjective]
+          team_counts[adjective] = count.nil? ? 1 : count + 1
+        end
+      end
+    end
+
+    def extract_adjectives(segment)
+      segment.apply :category
+      segment.adjectives.map { |a| a.to_s.downcase }
     end
   end
 end
