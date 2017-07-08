71d130a9cd026c400821300605577974a5a5ed2e
ainzzorl <ainzzorl@gmail.com>
2017-07-08 16:06:12 -0700

    Add a script that runs the entire process

---
+++ scripts/run.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+unless ARGV.length == 1
+  puts 'Usage: phase1.rb <path-to-input-file>'
+  puts 'The file must be a .csv with comment body in the first column and comment id in the second.'
+  exit 1
+end
+
+input_file_path = ARGV[0]
+
+FileUtils.mkdir_p './output/phase1'
+FileUtils.mkdir_p './output/phase2'
+FileUtils.mkdir_p './output/phase3'
+
+system 'bundle install'
+
+system "./scripts/phase1.rb #{input_file_path}"
+system './scripts/phase2.rb'
+system './scripts/phase3.rb'
